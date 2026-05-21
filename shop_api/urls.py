f26ec0d678219c6b1e9be09596485884d9fe36b5
Simon <s25887840@gmail.com>
2026-05-21 20:42:13 +0600

    hw_7

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from product.views import (
+    RegisterView,
+    LoginView,
+    ConfirmUserView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('product.urls')),
-    path('users/register/', RegisterView.as_view()),
-    path('users/login/', LoginView.as_view()),
-    path('users/confirm/', ConfirmUserView.as_view()),
+    #path('users/register/', RegisterView.as_view()),
+    #path('user/login/',LoginView.as_view()),
+    #path('users/confirm/', ConfirmUserView.as_view()),
     
 ]
